e1c4e60b073e5acb10d0ec886b64afe35a01a4df
Frank Bahrami <fbahramigold@gmail.com>
2019-03-17 15:30:46 -0700

    try again for aws

--- config/env.js
+++ config/env.js
@@ -2,7 +2,8 @@ const env = {
   database: 'sakila',
   username: 'root',
   password: '1Plokijuh',
-  host: 'localhost',
+  //host: 'localhost',
+  host: 'sakila.cu2ri2zgi9gr.us-west-1.rds.amazonaws.com',
   dialect: 'mysql',
   pool: {
     max: 5,
