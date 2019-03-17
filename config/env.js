9b2ac29069d959badd7d51e5923d5e4538672af8
Frank Bahrami <fbahramigold@gmail.com>
2019-03-17 15:52:44 -0700

    Changed timeout value and min connection

--- config/env.js
+++ config/env.js
@@ -7,9 +7,11 @@ const env = {
   dialect: 'mysql',
   pool: {
     max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    min: 1,
+    //acquire: 30000,
+    acquire: 300000,
+    //idle: 10000
+    idle: 100000
   }
 };
 
